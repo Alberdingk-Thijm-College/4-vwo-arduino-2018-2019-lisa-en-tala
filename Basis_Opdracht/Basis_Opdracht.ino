c8a134b0c9753f00c2e8be70aadddbfb0e3f87df
Tala16 <46483002+Tala16@users.noreply.github.com>
2019-01-22 11:48:21 +0100

    Update Basis_Opdracht.ino

--- Basis_Opdracht/Basis_Opdracht.ino
+++ Basis_Opdracht/Basis_Opdracht.ino
@@ -1,9 +1,50 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+
+void setup() {
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
 }
 
-void loop() 
-{
-  // put your main code here, to run repeatedly:
+void loop() {
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(8, HIGH);
+  delay(1000);
+  digitalWrite(1, HIGH);
+  digitalWrite(8, LOW);
+  delay(500); 
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH); 
+  delay(1000); 
+  digitalWrite(2, HIGH);
+  digitalWrite(9, LOW);
+  delay(500); 
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(1, HIGH);
+  delay(1000); 
+  digitalWrite(3, HIGH); 
+  digitalWrite(10, LOW);
+  delay(500);
+  digitalWrite(3, HIGH); 
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(1, HIGH);
+  delay(1000);
+  digitalWrite(4, HIGH);
+  digitalWrite(11, LOW);
+  delay(500);
 }
